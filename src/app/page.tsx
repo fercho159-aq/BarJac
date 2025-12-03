a4dd3c275f0a6b6937ccd02cd95cf135ff718fc4
MAW SOLUCIONES <solucionesmaw@gmail.com>
2025-12-03 18:13:56 +0000

    Necesito que  crees una categorias antes de bebidas que diga "Entradas"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,107 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const entranceItems = [
+    { name: "Coktel de Camaron Chico", quantity: "80 gr", accompaniment: "cebolla, jitomate, cilantro, aguacate", price: "95.00" },
+    { name: "Coktel de Camaron Grande", quantity: "140 gr", accompaniment: "cebolla, jitomate, cilantro, aguacate", price: "145.00" },
+    { name: "Coktel de Campechano Chico", quantity: "80 gr", accompaniment: "cebolla, jitomate, cilantro, aguacate", price: "95.00" },
+    { name: "Coktel de Campachano Grande", quantity: "140 gr", accompaniment: "cebolla, jitomate, cilantro, aguacate", price: "145.00" },
+    { name: "Queso Fundido", accompaniment: "Cebolla Cambray, chile toreado, tortillas", price: "125.00" },
+    { name: "Queso Fundido con Arrachera", accompaniment: "Cebolla Cambray, chile toreado, tortillas", price: "165.00" },
+    { name: "Queso Fundido con Ahuja Norteña", accompaniment: "Cebolla Cambray, chile toreado, tortillas", price: "165.00" },
+    { name: "Queso Fundido con Ribey", accompaniment: "Cebolla Cambray, chile toreado, tortillas", price: "185.00" },
+    { name: "Queso Fundido con New York", accompaniment: "Cebolla Cambray, chile toreado, tortillas", price: "185.00" },
+    { name: "Queso Fundido con Sirlon", accompaniment: "Cebolla Cambray, chile toreado, tortillas", price: "185.00" },
+    { name: "Guacamole", accompaniment: "Cebolla Cambray, chile toreado, tortillas", price: "145.00" },
+  ];
+
   const drinkItems = [
-    { name: "Gin & Tonic", price: "150", description: "Beefeater, Agua Tónica, Gajo de Limón Verde." },
-    { name: "Vodka & Tonic", price: "140", description: "Smirnoff, Agua Tónica, Gajo de Limón Verde." },
-    { name: "Cuba Libre", price: "130", description: "Bacardi Blanco, Jugo de Limón, Coca Cola." },
-    { name: "Tequila Sunrise", price: "160", description: "Jose Cuervo Tradicional, Jugo de Naranja, Granadina." },
-    { name: "Godfather", price: "180", description: "J.W. Red Label, Disaronno." },
-    { name: "Black Russian", price: "150", description: "Smirnoff, Kahlua." },
-    { name: "Cosmopolitan", price: "170", description: "Smirnoff, Controy, Jugo de Arándano, Jugo de Limón." },
-    { name: "Margarita Clásica", price: "160", description: "Jose Cuervo Tradicional, Controy, Jugo de Limón." },
+    { name: "Gin & Tonic", description: "Beefeater, Agua Tónica, Gajo de Limón Verde." },
+    { name: "Vodka & Tonic", description: "Smirnoff, Agua Tónica, Gajo de Limón Verde." },
+    { name: "Cuba Libre", description: "Bacardi Blanco, Jugo de Limón, Coca Cola." },
+    { name: "Cubata", description: "Bacardi Añejo, Jugo de Limón, Coca Cola." },
+    { name: "Tequila Sunrise", description: "Jose Cuervo Tradicional, Jugo de Naranja, Granadina." },
+    { name: "Godfather", description: "J.W. Red Label, Disaronno." },
+    { name: "Godmother", description: "Smirnoff, Disaronno." },
+    { name: "Black Russian", description: "Smirnoff, Kahlua." },
+    { name: "White Russian", description: "Smirnoff, Kahlua, Leche." },
+    { name: "Colorado Bulldog", description: "Smirnoff, Kahlua, Coca Cola." },
+    { name: "Woo Woo", description: "Smirnoff, Flamingo De Durazno, Jugo de Arándano." },
+    { name: "Cape Codder", description: "Smirnoff, Jugo De Arándano." },
+    { name: "Americano", description: "Campari, Vermut Dulce, Agua Mineral." },
+    { name: "Screaming Orgasm", description: "Smirnoff, Kahlua, Disaronno, Baileys, Leche." },
+    { name: "Orgasm", description: "Kahlua, Disaronno, Baileys, Leche." },
+    { name: "Sex On The Beach", description: "Smirnoff, Flamingo De Durazno, Jugo De Naranja, Jugo De Arándano." },
+    { name: "Cosmopolitan", description: "Smirnoff, Controy, Jugo de Arándano, Jugo de Limón." },
+    { name: "Daiquiri Clasico", description: "Bacardi Blanco, Jugo De Limon Verde, Jarabe Natural." },
+    { name: "Lychburg Limonade", description: "Jim Bean White, Controy, Jugo De Limon Amarillo, Jarabe Natural, Sprite." },
+    { name: "Horses Neck", description: "Jim Bean White, Bitter De Angostura, Ginger Ale." },
+    { name: "Tom Collins", description: "Beefetear, Jugo De Limón Amarillo, Jarabe Natural, Agua Mineral." },
+    { name: "John Collins", description: "Jim Bean White, Jugo De Limon Amarillo, Jarabe Natural, Agua Mineral." },
+    { name: "Long Island Iced Tea", description: "Smirnoff, Beefetear, Bacardi Blanco, Jose Cuervo Tradicional, Controy, Jarabe Natural, Jugo De Limon Amarillo, Coca Cola." },
+    { name: "Long Beach Iced Tea", description: "Smirnoff, Beefetear, Bacardi Blanco, Jose Cuervo Tradicional, Controy, Jarabe Natural, Jugo De Limon Amarillo, Jugo De Arándano." },
+    { name: "Beverly Hills Iced Tea", description: "Smirnoff, Beefetear, Bacardi Blanco, Jose Cuervo Tradicional, Controy, Jarabe Natural, Jugo De Limon Amarillo, Cinzano To Spritz." },
+    { name: "Dry Martini", description: "Smirnoff/Beefeater, Vermut Seco." },
+    { name: "Wet Martini", description: "Smirnoff/Beefeater, Vermut Seco." },
+    { name: "Dirty Martini", description: "Smirnoff/Beefeater, Vermut Seco, Salmuera." },
+    { name: "Manhattan Dulce", description: "Jim Bean White, Bitter De Angostura, Vermut Dulce." },
+    { name: "Manhattan Seco", description: "Jim Bean White, Bitter De Angostura, Vermut Seco." },
+    { name: "Manhattan Perfecto", description: "Jim Bean White, Bitter De Angostura, Vermut Dulce, Vermut Seco." },
+    { name: "Rob Roy", description: "J.W. Red Label, Vermut Dulce, Bitter De Angostura." },
+    { name: "Paloma", description: "Sal, Jose Cuervo Tradicional, Jugo De Toronja, Jugo De Limón Verde, Jarabe Natural, Agua Mineral." },
+    { name: "Margarita Clásica", description: "Sal, Jose Cuervo Tradicional, Controy, Jugo De Limón Verde, Jarabe Natural." },
+    { name: "Tommy's Margarita", description: "Sal, Jose Cuervo Tradicional, Jugo De Limón Verde, Miel De Agave." },
+    { name: "Side Car", description: "Azúcar, Martel V.S., Controy, Jugo De Limón Verde, Jarabe Natural." },
+    { name: "Amaretto Sour", description: "Bitter De Angostura, Clara De Huevo, Disaronno, Jugo De Limón Amarillo, Jarabe Natural." },
+    { name: "Midori Sour", description: "Bitter De Angostura, Clara De Huevo, Midori, Jugo De Limón Amarillo, Jarabe Natural." },
+    { name: "Whiskey Sour", description: "Bitter De Angostura, Clara De Huevo, Jim Bean White, Jugo De Limón Amarillo, Jarabe Natural." },
+    { name: "New York Sour", description: "Bitter De Angostura, Clara De Huevo, Jim Bean White, Jugo De Limón Amarillo, Jarabe Natural, Altozano Ensamble Crianza." },
+    { name: "Clover Club", description: "Beefetear, Jugo De Limón Amarillo, Néctar De Frambuesa, Clara De Huevo." },
+    { name: "Mojito", description: "Gajo De Limón Verde, Jarabe Natural, Hierbabuena, Bacardi Blanco, Agua Mineral." },
+    { name: "Caipiroska", description: "Gajo De Limón Verde, Jarabe Natural, Smirnoff." },
+    { name: "Gin Basil Smash", description: "Beefetear, Jugo De Limón Amarillo, Jarabe Natural, Albahaca." },
+    { name: "Southside", description: "Beefetear, Jugo De Limón Amarillo, Jarabe Natural, Hierbabuena." },
+    { name: "B52", description: "Kahlua, Baileys, Controy." },
+    { name: "Last Word", description: "Beefetear, Flamingo De Cereza, Chartreuse Verde, Jugo De Limón Verde." },
+    { name: "Old Fashioned", description: "Bitter De Angostura, Jim Bean White, Jarabe Natural." },
+    { name: "Fidel Castro", description: "Jugo De Limón Verde, Bacardi Añejo, Ginger Ale." },
+    { name: "Hemingway Daiquiri", description: "Bacardi Blanco, Flamingo De Cereza, Jugo De Limón Verde, Jugo De Toronja." },
+    { name: "White Lady", description: "Beefetear, Controy, Jugo De Limón Amarillo, Jarabe Natural." },
+    { name: "Pink Lady", description: "Beefetear, Controy, Jugo De Limón Amarillo, Jarabe Natural, Granadina." },
+    { name: "Bloody Mari", description: "Smirnoff, Jugo De Tomate, Petroleo, Sal, Pimienta Negra, Jugo De Limón Verde." },
+    { name: "Red Snapper", description: "Beefetear, Jugo De Tomate, Petroleo, Sal, Pimienta Negra, Jugo De Limón Verde." },
+    { name: "Negroni", description: "Beefetear, Vermut Dulce, Campari." },
+    { name: "Negroni Sbagliato", description: "Cinzano To Spritz, Vermut Dulce, Campari." },
+    { name: "Boulevardier", description: "Jim Bean White, Vermut Dulce, Campari." },
+    { name: "Aperol Spritz", description: "Cinzano To Spritz, Aperol, Agua Mineral." },
+    { name: "Mimosa", description: "Jugo De Naranja, Cinzano To Spritz." },
+    { name: "Expresso Martini", description: "Smirnoff, Kahlua, Expresso, Jarabe Natural." },
+    { name: "Kamikaze", description: "Smirnoff, Controy, Jugo De Limón Verde." },
+    { name: "Pink Gin", description: "Beefetear, Bitter De Angostura." },
+    { name: "Piña Colada", description: "Jugo De Piña, Bacardi Blanco, Flamingo De Coco, Jugo De Limón Verde, Néctar De Coco." },
+    { name: "Lemon Drop", description: "Smirnoff, Jugo De Limón Amarillo, Jarabe Natural." },
+    { name: "Bull", description: "Jugo De Limón Verde, Jarabe Natural, Bacardi Blanco, Bohemia Oscura." },
+    { name: "Mezcalita", description: "Tajín, Mono Araña, Jugo De Limón Verde, Jarabe Natural." },
+    { name: "MezcalOni", description: "Mono Araña, Vermut Dulce, Campari." },
+    { name: "Carajillo", description: "Licor 43, Expresso." },
+    { name: "Carajillo con kalua", description: "Licor 43, Kahlua, Expresso." },
+    { name: "Tinto De Verano", description: "Sprite, Altozano Ensamble Crianza." },
+    { name: "Pitufo", description: "Malibu, Flamingo De Curazao, Jugo De Piña, Sprite." },
+    { name: "Back Room", description: "Beefetear, Vermut Seco, Frangelico." },
+    { name: "Frangelico Spritz", description: "Cinzano To Spritz, Frangelico, Agua Mineral." },
+    { name: "St Germain Spritz", description: "Cinzano To Spritz, St Germain, Agua Mineral." },
+    { name: "AMF", description: "Smirnoff, Beefetear, Mono Araña, Flamingo De Curazao, Jugo De Limon Amarillo, Jarabe Natural, Sprite." },
+    { name: "Cerveza Sunrise", description: "Chamoy De Sabor, Tajin, Jose Cuervo Tradicional, Granadina, Jugo De Limón Verde, Heineken." },
+    { name: "Cerveza Hawaian", description: "Chamoy De Sabor, Tajin, Bacardi Blanco, Jugo De Piña, Flamingo De Curazao, Jugo De Limón Verde, Heineken." },
+    { name: "Chela Azulita", description: "Chamoy De Sabor, Tajin, Jose Cuervo Tradicional, Flamingo De Curazao, Jugo De Limón Verde, Sprite." },
+    { name: "Chelada", description: "Sal, Jugo De Limón Verde." },
+    { name: "Cubana", description: "Tajin, Jugo De Limón Verde, Petroleo." },
+    { name: "Clamato", description: "Tajin, Jugo De Limón Verde, Petroleo, Clamato." },
+    { name: "Coctel De Camarones", description: "Tajin, Camarones, Jugo De Limón Verde, Petroleo, Clamato." },
+    { name: "Coctel De Ostiones", description: "Tajin, Ostiones, Jugo De Limón Verde, Petroleo, Clamato." },
   ];
 
   const foodItems = [
-    { name: "Alitas BBQ", price: "150", description: "10 pz de alitas bañadas en nuestra salsa BBQ casera." },
-    { name: "Hamburguesa Jack", price: "180", description: "Carne de res premium, queso cheddar, tocino y aros de cebolla." },
-    { name: "Tacos de Arrachera", price: "120", description: "3 tacos de arrachera con guacamole y cebolla morada." },
     { name: "Tostada de Atun", quantity: "1 pz 80 gr", accompaniment: "cebolla, jitomate, aguacate", price: "95.00" },
     { name: "Tostada de Pescado", quantity: "1 pz 60 gr", accompaniment: "cebolla, jitomate, aguacate", price: "95.00" },
     { name: "Pescadilla", quantity: "3 pz 500 gr", accompaniment: "Aguacate, limon", price: "83.00" },
@@ -30,7 +116,7 @@ export default function Home() {
     { name: "Arrachera", quantity: "200 gr", accompaniment: "Papas a la Francesa, Cebollas Cambray, Nopal Baby, Chiles toreados", price: "295.00" },
     { name: "Tacos de Arrachera (Orden)", quantity: "1 pz 60 gr", accompaniment: "Papas al oregano, chiles toreados, nopal baby, guacamole", price: "58.00" },
     { name: "Hamburguesa Clasica", quantity: "1 pz 180 gr", accompaniment: "Queso americano. Lechuga, jitomate, cebolla, tocino, papas a la francesa", price: "175.00" },
-];
+  ];
 
   const promos = [
     { title: "Jueves de Amigos", description: "2x1 en toda la coctelería nacional." },
@@ -78,6 +164,22 @@ export default function Home() {
           <div className="container">
             <h2 className="text-3xl font-bold text-center mb-10">Nuestro Menú</h2>
             
+            <h3 className="text-2xl font-bold text-center mb-6 mt-12">Entradas</h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+              {entranceItems.map((item, index) => (
+                <Card key={index}>
+                  <CardHeader>
+                    <CardTitle>{item.name}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    {item.quantity && <p className="text-sm text-muted-foreground">{item.quantity}</p>}
+                    <p className="text-muted-foreground mb-2">{item.accompaniment}</p>
+                    <p className="font-semibold text-primary">${item.price}</p>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+
             <h3 className="text-2xl font-bold text-center mb-6 mt-12">Bebidas</h3>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
               {drinkItems.map((item, index) => (
@@ -87,7 +189,6 @@ export default function Home() {
                   </CardHeader>
                   <CardContent>
                     <p className="text-muted-foreground mb-2">{item.description}</p>
-                    <p className="font-semibold text-primary">${item.price}</p>
                   </CardContent>
                 </Card>
               ))}
@@ -102,8 +203,7 @@ export default function Home() {
                   </CardHeader>
                   <CardContent>
                     {item.quantity && <p className="text-sm text-muted-foreground">{item.quantity}</p>}
-                    {item.accompaniment && <p className="text-muted-foreground mb-2">{item.accompaniment}</p>}
-                    {item.description && <p className="text-muted-foreground mb-2">{item.description}</p>}
+                    <p className="text-muted-foreground mb-2">{item.accompaniment}</p>
                     <p className="font-semibold text-primary">${item.price}</p>
                   </CardContent>
                 </Card>
